2b7b696e10ae5b9fa6568903efeba23c892dbfc0
Ivan L. Seibel <me@ivanseibel.dev>
2021-06-06 12:27:40 -0300

    feat: add capability to deal with json data

--- network/views.py
+++ network/views.py
@@ -1,6 +1,7 @@
 import json
 from django.contrib.auth import authenticate, login, logout
 from django.http.response import JsonResponse
+# from django.http import JsonResponse
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
