7d4d59b8bbfe1540a3dbe105ff5d3245066c72e1
Ivan L. Seibel <me@ivanseibel.dev>
2021-06-11 23:16:05 -0300

    chore: register like model

--- network/admin.py
+++ network/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from network.models import Following, User, Post
+from network.models import Following, Like, User, Post
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Following)
+admin.site.register(Like)
